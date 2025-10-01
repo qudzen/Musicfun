b4e331a9faf953d36bcaa575a1b4475cb3738958
vvelt <pavlovvasa860@gmail.com>
2025-10-01 11:51:12 +1000

    Второй коммит

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,52 @@
 import './App.css'
+import {useState} from "react";
 
 const tracks = [
-    {title: 'Musiccfun soundtrack', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
-    {title: 'Musicfun', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'}
+    {id: '1', isSelected: true, title: 'Musiccfun soundtrack', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
+    {id: '2', isSelected: false, title: 'Musicfun', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'}
 ]
 
-const newLiReactElements = tracks.map((track) => {
-    return (
-        <li>
-            <div>
-                {track.title}
-            </div>
-            <audio controls src={tracks[0].url}></audio>
-        </li>)
-})
+// const newLiReactElements = tracks.map((track) => {
+//     return (
+//         <li key={track.id} style = { {border: track.isSelected ? '1px solid red' : 'none'} }>
+//             <div>
+//                 {track.title}
+//             </div>
+//             <audio controls src={track.url}></audio>
+//         </li>)
+// })
 
 
 function App() {
+    const [selectedTrackId, setSelectedTrackId] = useState(null)
 
+    if (tracks.length === 0){
+        return (
+            <div>
+                <h1>Musicfun</h1>
+                <span>No tracks</span>
+            </div>
+        )
+    }
   return (
     <>
         <div>
             <h1>Musicfun player</h1>
             <ul>
-                {newLiReactElements}
+                {
+                    tracks.map((track) => {
+                        return (
+                            <li key={track.id} style={{border: track.id ===  selectedTrackId ? '1px solid red' : 'none'}}>
+                                <div onClick={ () => {
+                                    setSelectedTrackId(track.id)
+                                }}>
+                                    {track.title}
+                                </div>
+                                <audio controls src={track.url}></audio>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </div>
     </>
